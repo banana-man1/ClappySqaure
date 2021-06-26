d8b6a1e7c7ab32f599d5430d35e20b19ac9bcdf8
Crostini <git@github>
2021-06-26 19:49:41 +1200

    7:49PM

--- newVars.js
+++ newVars.js
@@ -1,7 +1,43 @@
-var myGameArea, speed, myGamePiece, myObstacles, myScore;
+var myGameArea, speed, myGamePiece, myObstacles, myScore, vaild, d, myObstacles, licenseCodes;
 myObstacles = []
 speed = 0.05
 gravity = 0.05
+d = new Date()
+vaild = false
+licenseCodes = [
+	"Q1MtS0JFODBURTk=",
+	"Q1MtMk9KU09HUkg=",
+	"Q1MtTldZM1NRVEo=",
+	"Q1MtUlRETVBQMTc=",
+	"Q1MtU0E5VUFFNlg=",
+	"Q1MtWUNYTVhDSVY=",
+	"Q1MtSzgwUlVWMjQ=",
+	"Q1MtOVBBMkxaMzY=",
+	"Q1MtQUQzQUtHUjU=",
+	"Q1MtWkhBRlJJSjI=",
+	"Q1MtTU5DTDZDQk8=",
+	"Q1MtNzhPTkhFRzE=",
+	"Q1MtS1lZWUtIOUU=",
+	"Q1MtSzlaRUIzWEo=",
+	"Q1MtSkg4QVNSMjE=",
+	"Q1MtOThBR0U2Q1o=",
+	"Q1MtQ0Y1TzMzTks=",
+	"Q1MtUDFYQTlHU1E=",
+	"Q1MtTUJXTUlPRkQ=",
+	"Q1MtTDVTUlhQRkQ=",
+	"Q1MtVkpaQlhGTUI=",
+	"Q1MtSFpGUElXRDU=",
+	"Q1MtTTJMOFFJOTk=",
+	"Q1MtU0tJVzdBWVo=",
+	"Q1MtVUdaUVNLSVg=",
+	"Q1MtWlBYVFM1NTc=",
+	"Q1MtRVZVTVlDVFk=",
+	"Q1MtVjZEUFhCNjE=",
+	"Q1MtWFpYWllTWFU=",
+	"Q1MtV0kxR05NS0c=",
+	"Q1MtVEVTVA=="
+]
+
 
 myGameArea = {
 	canvas: document.createElement("canvas"),
